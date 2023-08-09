062316de742c11a7cc71844fa17ea09291f36911
karina10004 <gs2110004@sgsitsindore.in>
2023-08-09 13:51:04 +0530

    frontend

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://media.samyakk.com/tagalys/product_images/n/a/na2126-a.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+      cat:"women"
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+      cat:"coat"
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+      cat:"jeans"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://th.bing.com/th/id/OIP.4ATifL4Lju8__M53IrawJwHaLG?w=202&h=303&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    },
+    {
+      id:2,
+      img:"https://th.bing.com/th/id/OIP.8neKyVT0JiO3e-7TdLgOGwHaMV?w=202&h=319&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://th.bing.com/th/id/OIP.HCxdrgZAN8R96OfqsWhcbgHaJo?w=202&h=263&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://th.bing.com/th?id=OPA.wR9199F9PDcQWw474C474&w=220&h=210&o=5&pid=21.1",
+    },
+    {
+      id:8,
+      img:"https://th.bing.com/th?id=OPA.yDyYibKMlKl3yw474C474&w=220&h=210&o=5&pid=21.1",
+    },
+  ]
